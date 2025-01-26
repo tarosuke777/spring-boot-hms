c5376fba5dbab8dc345cadc8cf64af33df90c8d6
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-01-26 17:35:07 +0900

    ログイン画面の場合、サイドメニューを非表示

--- src/main/java/com/tarosuke777/hms/controller/LoginController.java
+++ src/main/java/com/tarosuke777/hms/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.tarosuke777.hms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(Model model) {
+		model.addAttribute("fromLoginDisplay", "1");
 		return "login";
 	}
 }
