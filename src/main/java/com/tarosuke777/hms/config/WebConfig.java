f16dc1c93452fcff1c6bc43304cf27b362d076fc
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-05-18 15:24:20 +0900

    refactor: add @Nonull

--- src/main/java/com/tarosuke777/hms/config/WebConfig.java
+++ src/main/java/com/tarosuke777/hms/config/WebConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.lang.NonNull;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
+	public void addViewControllers(@NonNull ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("redirect:/music/list");
 	}
 }
