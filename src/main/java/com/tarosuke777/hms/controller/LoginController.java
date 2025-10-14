67ab1a577de08a5eef4a33dc563a0743d662578c
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-10-14 09:00:54 +0900

    ログイン済の場合、ログイン後画面に自動遷移

--- src/main/java/com/tarosuke777/hms/controller/LoginController.java
+++ src/main/java/com/tarosuke777/hms/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.tarosuke777.hms.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,17 @@ public class LoginController {
 
 	@GetMapping("/login")
 	public String login(Model model) {
+
+		// 現在の認証情報を取得
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		// 認証情報が存在し、匿名ユーザーではない（＝ログイン済み）ことを確認
+		if (authentication != null && authentication.isAuthenticated()
+				&& !(authentication instanceof AnonymousAuthenticationToken)) {
+			log.info("既にログイン済みのため、ログイン成功時のデフォルトURLへリダイレクトします。");
+			return "redirect:/task/list";
+		}
+
 		model.addAttribute("isNotDisplayMenu", true);
 		return "login";
 	}
