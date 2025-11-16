f2af12f4048e1ea55f628f1cc836724bfe0e1550
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-11-16 18:20:43 +0900

    Remove unused import in McpSecurityConfig for cleaner code

--- src/main/java/com/tarosuke777/hms/config/McpSecurityConifg.java
+++ src/main/java/com/tarosuke777/hms/config/McpSecurityConifg.java
@@ -1,6 +1,5 @@
 package com.tarosuke777.hms.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
