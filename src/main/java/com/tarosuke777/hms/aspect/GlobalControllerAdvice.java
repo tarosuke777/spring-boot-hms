522d06417a3d38f056de2dbacd32d8085236e01f
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-01-10 19:52:57 +0900

    エラーハンドリングを修正

--- src/main/java/com/tarosuke777/hms/aspect/GlobalControllerAdvice.java
+++ src/main/java/com/tarosuke777/hms/aspect/GlobalControllerAdvice.java
@@ -1,20 +1,28 @@
 package com.tarosuke777.hms.aspect;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
+import java.util.UUID;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-	@ExceptionHandler({ Exception.class })
-	public String testExceptionHandle(Exception e, Model model) {
-		String stackTrace = ExceptionUtils.getStackTrace(e);
-		model.addAttribute("exception", e.getClass());
-		model.addAttribute("stackTrace", stackTrace);
-		
+	@ExceptionHandler({Exception.class})
+	public String handleException(Exception e, Model model) {
+		// ユニークなIDを発行
+		String errorId = UUID.randomUUID().toString();
+
+		// ログにはスタックトレースとIDを紐づけて出力（開発者はこれを見る）
+		log.error("Error ID: {}", errorId, e);
+
+		// 画面にはIDとクラス名程度に留める
+		model.addAttribute("errorId", errorId);
+		model.addAttribute("exception", e.getClass().getSimpleName());
 		model.addAttribute("isNotDisplayMenu", true);
+
 		return "error";
 	}
 
