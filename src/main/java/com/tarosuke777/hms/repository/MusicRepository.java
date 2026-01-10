2bee415cc7b4a8d3d84116f0552e02087e4ccbf9
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-01-10 15:48:08 +0900

    @NonNullを追加

--- src/main/java/com/tarosuke777/hms/repository/MusicRepository.java
+++ src/main/java/com/tarosuke777/hms/repository/MusicRepository.java
@@ -3,6 +3,7 @@ package com.tarosuke777.hms.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 import com.tarosuke777.hms.entity.MusicEntity;
 
@@ -13,6 +14,8 @@ public interface MusicRepository extends JpaRepository<MusicEntity, Integer> {
      * findMany に相当<br>
      * ArtistEntity を Fetch Join して効率的に全件取得する
      */
+    @Override
+    @NonNull
     @EntityGraph(attributePaths = {"artist"})
     List<MusicEntity> findAll();
 }
