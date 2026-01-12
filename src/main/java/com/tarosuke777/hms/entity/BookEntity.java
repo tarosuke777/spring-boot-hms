266c48d480abdaec8e1a160929e053c7602a254b
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-01-12 11:56:11 +0900

    fix import

--- src/main/java/com/tarosuke777/hms/entity/BookEntity.java
+++ src/main/java/com/tarosuke777/hms/entity/BookEntity.java
@@ -1,6 +1,5 @@
 package com.tarosuke777.hms.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
