941105cc116ecba576e4c501b0d3c400fbb98e89
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-01-15 08:20:33 +0900

    不要なインポート文を削除

--- src/test/java/com/tarosuke777/hms/controller/ArtistControllerTest.java
+++ src/test/java/com/tarosuke777/hms/controller/ArtistControllerTest.java
@@ -7,14 +7,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.TestSecurityContextHolder;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
