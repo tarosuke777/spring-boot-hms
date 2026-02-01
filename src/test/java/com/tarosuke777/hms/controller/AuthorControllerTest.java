05b51444dbd7eee4e77be1087377488bc43b672d
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-02-02 08:28:30 +0900

    不要なインポート文を削除

--- src/test/java/com/tarosuke777/hms/controller/AuthorControllerTest.java
+++ src/test/java/com/tarosuke777/hms/controller/AuthorControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
