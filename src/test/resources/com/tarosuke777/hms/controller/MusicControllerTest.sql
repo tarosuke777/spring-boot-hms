b60c7402227d9419fc2a10d78be70783a4878e50
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-02-01 18:07:48 +0900

    UTエラーを解消

--- src/test/resources/com/tarosuke777/hms/controller/MusicControllerTest.sql
+++ src/test/resources/com/tarosuke777/hms/controller/MusicControllerTest.sql
@@ -21,14 +21,13 @@ VALUES
 
 -- Test Artist A に紐づく曲
 INSERT INTO music (music_name, artist_id, created_at, updated_at, created_by, updated_by, version)
-SELECT 'Test Track 01 - A', artist_id, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), 'admin', 'admin', 0 
-FROM artist WHERE artist_name = 'Test Artist A';
+SELECT 'Test Track 01 - A', id, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), 'admin', 'admin', 0 
+FROM artist WHERE name = 'Test Artist A';
 
 INSERT INTO music (music_name, artist_id, created_at, updated_at, created_by, updated_by, version)
-SELECT 'Test Track 02 - A', artist_id, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), 'admin', 'admin', 0 
-FROM artist WHERE artist_name = 'Test Artist A';
-
+SELECT 'Test Track 02 - A', id, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), 'admin', 'admin', 0 
+FROM artist WHERE name = 'Test Artist A';
 -- Test Artist B に紐づく曲
 INSERT INTO music (music_name, artist_id, created_at, updated_at, created_by, updated_by, version)
-SELECT 'Test Track 03 - B', artist_id, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), 'user', 'user', 0 
-FROM artist WHERE artist_name = 'Test Artist B';
+SELECT 'Test Track 03 - B', id, CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6), 'user', 'user', 0 
+FROM artist WHERE name = 'Test Artist B';
