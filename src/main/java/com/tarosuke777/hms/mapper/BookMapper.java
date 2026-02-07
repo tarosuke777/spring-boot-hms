faaaa5b80e4bc0d4db7d009bc3b404fc21ef1503
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-02-07 16:46:48 +0900

    MapStructで未マッピングはビルドエラーに変更 及び 未マッピング項目に無視のアノテーションを追加

--- src/main/java/com/tarosuke777/hms/mapper/BookMapper.java
+++ src/main/java/com/tarosuke777/hms/mapper/BookMapper.java
@@ -1,6 +1,7 @@
 package com.tarosuke777.hms.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import com.tarosuke777.hms.entity.BookEntity;
 import com.tarosuke777.hms.form.BookForm;
@@ -9,13 +10,17 @@ import com.tarosuke777.hms.form.BookForm;
 public interface BookMapper {
 
     @IgnoreAuditFields
+    @Mapping(target = "author", ignore = true)
     BookEntity toEntity(BookForm form);
 
+    @Mapping(target = "authorId", ignore = true)
     BookForm toForm(BookEntity entity);
 
+    @Mapping(target = "author", ignore = true)
     BookEntity copy(BookEntity entity);
 
     @IgnoreAuditFields
+    @Mapping(target = "author", ignore = true)
     void updateEntityFromForm(BookForm form, @MappingTarget BookEntity entity);
 
 }
