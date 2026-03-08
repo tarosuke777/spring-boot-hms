5eeac8d6908144938a8da47260a5eff4fae353df
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-03-08 19:19:01 +0900

    不要なインポートを削除

--- src/main/java/com/tarosuke777/hms/controller/ArtistController.java
+++ src/main/java/com/tarosuke777/hms/controller/ArtistController.java
@@ -1,7 +1,6 @@
 package com.tarosuke777.hms.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
