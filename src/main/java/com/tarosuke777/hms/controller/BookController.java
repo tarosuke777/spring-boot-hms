c078fc1785ec3479a18ae969ba385b56a0879729
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-03-13 09:44:47 +0900

    不要なインポートを削除

--- src/main/java/com/tarosuke777/hms/controller/BookController.java
+++ src/main/java/com/tarosuke777/hms/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.tarosuke777.hms.controller;
 
-import java.util.List;
 import java.util.Map;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
