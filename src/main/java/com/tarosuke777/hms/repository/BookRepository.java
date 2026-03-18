2635819921e9df62648e853da9a713209a3f68bd
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-03-19 08:23:59 +0900

    警告を除去

--- src/main/java/com/tarosuke777/hms/repository/BookRepository.java
+++ src/main/java/com/tarosuke777/hms/repository/BookRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import com.tarosuke777.hms.entity.BookEntity;
 
@@ -16,7 +18,9 @@ public interface BookRepository
         extends JpaRepository<BookEntity, Integer>, JpaSpecificationExecutor<BookEntity> {
 
     @EntityGraph(attributePaths = {"author"})
-    Page<BookEntity> findAll(Specification<BookEntity> spec, Pageable pageable);
+    @Override
+    @NonNull
+    Page<BookEntity> findAll(@Nullable Specification<BookEntity> spec, @NonNull Pageable pageable);
 
     @EntityGraph(attributePaths = {"author"})
     Optional<BookEntity> findByIdAndCreatedBy(Integer id, Integer createdBy);
