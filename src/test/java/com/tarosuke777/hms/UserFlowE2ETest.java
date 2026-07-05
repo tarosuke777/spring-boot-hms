8f36dcd73f99420b3157ccd63919384d11ae2845
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-07-05 21:27:56 +0900

    E2Eテストを追加

--- src/test/java/com/tarosuke777/hms/UserFlowE2ETest.java
+++ src/test/java/com/tarosuke777/hms/UserFlowE2ETest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import com.microsoft.playwright.Browser;
@@ -17,6 +18,7 @@ import com.microsoft.playwright.Playwright;
 
 @Sql
 @Transactional
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserFlowE2ETest {
 
