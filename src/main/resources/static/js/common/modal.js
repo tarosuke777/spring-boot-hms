9f0ca87693e04f7e0e3bbd991c1fda7863c10ca2
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-07-07 20:46:26 +0900

    モーダルの共通化

--- src/main/resources/static/js/common/modal.js
+++ src/main/resources/static/js/common/modal.js
@@ -2,11 +2,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const table = document.querySelector('table'); // テーブル要素
     table.addEventListener('click', (event) => {
-        // クリックされた要素が .note-trigger か確認
-        const target = event.target.closest('.note-trigger');
+        // クリックされた要素が .modal-trigger か確認
+        const target = event.target.closest('.modal-trigger');
         if (target) {
-            const noteText = target.getAttribute('data-note');
-            document.getElementById('modalNoteBody').textContent = noteText;
+            const modalText = target.getAttribute('data-modal-text');
+            document.getElementById('globalModalBody').textContent = modalText;
         }
     });
 });
