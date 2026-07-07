749819f2b4b6be31ddc3cb92b99aeb4a8e9b0e24
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-07-07 21:24:48 +0900

    空白対応

--- src/main/resources/static/js/common/modal.js
+++ src/main/resources/static/js/common/modal.js
@@ -4,15 +4,15 @@ document.addEventListener('click', (event) => {
     const target = event.target.closest('.modal-trigger');
     if (!target) return; // なければ何もしない
 
-    const modalBody = target.getAttribute('data-modal-text');
+    const modalBody = target.getAttribute('data-modal-text') || '';
     const modalBodyElm = document.getElementById('globalModalBody');
-    if (modalBodyElm && modalBody) {
+    if (modalBodyElm) {
         modalBodyElm.textContent = modalBody;
     }
 
-    const modalTitle = target.getAttribute('data-modal-title');
+    const modalTitle = target.getAttribute('data-modal-title') || '';
     const modalTitleElm = document.getElementById('globalModalTitle');
-    if (modalTitleElm && modalTitle) {
+    if (modalTitleElm) {
         modalTitleElm.textContent = modalTitle;
     }
 });
