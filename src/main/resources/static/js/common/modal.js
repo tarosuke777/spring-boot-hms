92bd62ef6636a1a5ed4b1c36abd7e16a5644ec64
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-07-11 23:18:12 +0900

    configure repository settings

--- src/main/resources/static/js/common/modal.js
+++ src/main/resources/static/js/common/modal.js
@@ -1,18 +1,17 @@
 // document全体に対して1つだけリスナーを登録する（安全・汎用的）
-document.addEventListener('click', (event) => {
+document.addEventListener("click", (event) => {
+  const target = event.target.closest(".modal-trigger");
+  if (!target) return; // なければ何もしない
 
-    const target = event.target.closest('.modal-trigger');
-    if (!target) return; // なければ何もしない
+  const modalBody = target.getAttribute("data-modal-text") || "";
+  const modalBodyElm = document.getElementById("globalModalBody");
+  if (modalBodyElm) {
+    modalBodyElm.textContent = modalBody;
+  }
 
-    const modalBody = target.getAttribute('data-modal-text') || '';
-    const modalBodyElm = document.getElementById('globalModalBody');
-    if (modalBodyElm) {
-        modalBodyElm.textContent = modalBody;
-    }
-
-    const modalTitle = target.getAttribute('data-modal-title') || '詳細';
-    const modalTitleElm = document.getElementById('globalModalTitle');
-    if (modalTitleElm) {
-        modalTitleElm.textContent = modalTitle;
-    }
+  const modalTitle = target.getAttribute("data-modal-title") || "詳細";
+  const modalTitleElm = document.getElementById("globalModalTitle");
+  if (modalTitleElm) {
+    modalTitleElm.textContent = modalTitle;
+  }
 });
