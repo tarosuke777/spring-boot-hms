2aba9b1716a67317b93ab67ff925de3a4c91cec6
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-07-12 13:58:34 +0900

    EntityGraph を追加

--- src/main/java/com/tarosuke777/hms/repository/MovieRepository.java
+++ src/main/java/com/tarosuke777/hms/repository/MovieRepository.java
@@ -5,16 +5,20 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<MovieEntity, Integer> {
 
+  @EntityGraph(attributePaths = {"cast"})
   Optional<MovieEntity> findByIdAndCreatedBy(Integer id, Integer createdBy);
 
+  @EntityGraph(attributePaths = {"cast"})
   List<MovieEntity> findByCreatedBy(Integer createdBy);
 
+  @EntityGraph(attributePaths = {"cast"})
   Page<MovieEntity> findByCreatedBy(Integer createdBy, Pageable pageable);
 
   boolean existsByIdAndCreatedBy(Integer id, Integer createdBy);
