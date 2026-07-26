02a151e4854da569f6c1472fb538e14af4e4b583
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-07-26 10:15:23 +0000

    不要なクラスを除去

--- src/test/java/com/tarosuke777/hms/controller/MovieControllerTest.java
+++ src/test/java/com/tarosuke777/hms/controller/MovieControllerTest.java
@@ -116,54 +116,6 @@ public class MovieControllerTest {
   }
 
   @Test
-  void getRegister_WithCastId_ShouldPreselectCast() throws Exception {
-    // Given
-    CastEntity cast = castRepository.findAll().getFirst();
-    Integer castId = cast.getId();
-
-    // When & Then
-    mockMvc
-        .perform(get(REGISTER_ENDPOINT).param("castId", castId.toString())
-            .accept(MediaType.TEXT_HTML).characterEncoding("UTF-8"))
-        .andExpect(status().isOk()).andExpect(view().name(REGISTER_VIEW))
-        .andExpect(model().attributeExists("movieForm")).andDo(result -> {
-          MovieForm form = (MovieForm) result.getModelAndView().getModel().get("movieForm");
-          Assertions.assertEquals(castId, form.getCastId());
-        });
-  }
-
-  @Test
-  void getRegister_WithCastName_ShouldPreselectCast() throws Exception {
-    // Given
-    CastEntity cast = castRepository.findAll().getFirst();
-    String castName = cast.getName();
-    Integer expectedCastId = cast.getId();
-
-    // When & Then
-    mockMvc
-        .perform(get(REGISTER_ENDPOINT).param("castName", castName).accept(MediaType.TEXT_HTML)
-            .characterEncoding("UTF-8"))
-        .andExpect(status().isOk()).andExpect(view().name(REGISTER_VIEW))
-        .andExpect(model().attributeExists("movieForm")).andDo(result -> {
-          MovieForm form = (MovieForm) result.getModelAndView().getModel().get("movieForm");
-          Assertions.assertEquals(expectedCastId, form.getCastId());
-        });
-  }
-
-  @Test
-  void getRegister_WithInvalidCastName_ShouldNotPreselectCast() throws Exception {
-    // When & Then
-    mockMvc
-        .perform(get(REGISTER_ENDPOINT).param("castName", "NonExistentCastName123")
-            .accept(MediaType.TEXT_HTML).characterEncoding("UTF-8"))
-        .andExpect(status().isOk()).andExpect(view().name(REGISTER_VIEW))
-        .andExpect(model().attributeExists("movieForm")).andDo(result -> {
-          MovieForm form = (MovieForm) result.getModelAndView().getModel().get("movieForm");
-          Assertions.assertNull(form.getCastId());
-        });
-  }
-
-  @Test
   void register_WithValidData_ShouldRedirectToList() throws Exception {
 
     // Given
