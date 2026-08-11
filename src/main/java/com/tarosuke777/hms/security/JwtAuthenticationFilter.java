ac4ad80e1e2435a4a25fcb2bc816e0f56c88e281
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-08-11 07:06:10 +0000

    refactor

--- src/main/java/com/tarosuke777/hms/security/JwtAuthenticationFilter.java
+++ src/main/java/com/tarosuke777/hms/security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,16 +14,11 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtTokenProvider tokenProvider;
   private final UserDetailsService userDetailsService;
-
-  public JwtAuthenticationFilter(JwtTokenProvider tokenProvider,
-      UserDetailsService userDetailsService) {
-    this.tokenProvider = tokenProvider;
-    this.userDetailsService = userDetailsService;
-  }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
