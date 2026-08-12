921bfa87264c9d35d73c891dd83c935744887322
tarosuke <felis.silvestris.catus.7777@gmail.com>
2026-08-12 06:44:57 +0000

    add profile

--- src/main/java/com/tarosuke777/hms/config/OpenApiConfig.java
+++ src/main/java/com/tarosuke777/hms/config/OpenApiConfig.java
@@ -9,7 +9,9 @@ import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @Configuration
 public class OpenApiConfig {
 
